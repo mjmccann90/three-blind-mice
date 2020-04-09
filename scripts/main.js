73b542e51c62500f6414761a174ffa55b2177885
Mark McCann <mjmccann12@me.com>
2020-04-09 09:25:54 -0500

    main js imports

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+import { EmployeeList } from "./employees/EmployeeList.js"
+import { getEmployees } from "./employees/EmployeeProvider.js"
+import { getComputers } from "./computers/ComputerProvider.js"
+import { getDepartments } from "./departments/DepartmentProvider.js"
+import { getLocations } from "./locations/LocationProvider.js"
+
+getEmployees()
+    .then(getComputers)
+    .then(getLocations)
+    .then(getDepartments)
+    .then(EmployeeList)
